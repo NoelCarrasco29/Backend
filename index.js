41f007f0a81fd987b989f36dc750a2e6d8b2889f
Noel Carrasco <carrascon3055@gmail.com>
2022-04-27 08:56:47 -0600

    Hola mundo HTTP

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const http= require('http');
+
+const hostname='192.168.50.124';
+
+const port = 3000;
+
+const server=http.createServer((req, res) => {
+res.statusCode=200;
+
+res.setHeader('Content-Type', 'text/plain');
+
+res.end('Hola Mundo\n');
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`);
+});
